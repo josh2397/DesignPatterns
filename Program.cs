9d3f2b0f074a68f1a08079876bffc812bfcdd264
joshua mitchell <josh23797@gmail.com>
2022-06-20 23:59:39 +1000

    Initial factory pattern commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// Create a template for each design pattern
+// Create 2 examples for each
+// Ensure that all follow solid principles
+// Create unit tests for all patterns
+
+// Be able to interface with each pattern, specifying parameters where applicable for creation patterns etc.
+
+class Program {
+
+    static void Main(string[] args)
+    {
+        // Get input for which pattern to interact with
+
+        Console.WriteLine("Enter the name of the pattern to interact with or enter the corresponding number:\n");
+        Console.WriteLine("1: Factory");
+        Console.WriteLine("exit");
+    
+    }
+
+    static void CallDesignPattern(string input)
+    {
+        switch (input)
+        {
+            case "factory":
+            case "1":
+                
+                break;
+
+        }
+    }
+}
+
+
+
